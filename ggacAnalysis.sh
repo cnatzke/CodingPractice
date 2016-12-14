15d66884158982991335d1ad29ee84dbaaf40f64
cnatzke <natzke41@hotmail.com>
2016-12-14 13:34:16 -0700

    Version 1.1

--- ggacAnalysis.sh
+++ ggacAnalysis.sh
@@ -3,5 +3,18 @@
 # Generate angCorr.C macro for specific beam diameter
 python replace2.py
 
+echo "___________________________"
+echo "Starting the Analysis"
+echo "___________________________"
 # Run analysis
 grsisort -l -q angCorr.C
+
+echo "___________________________"
+echo "Cleaning up"
+echo "___________________________"
+
+rm AutoDict*
+
+echo "___________________________"
+echo "Done"
+echo "___________________________"
