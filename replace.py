f3c470e58cb8b73215dfa0e36a52bd0ca6a6fe99
cnatzke <natzke41@hotmail.com>
2016-12-17 15:18:29 +0100

    Version 2.0

--- replace.py
+++ replace.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
+import fileinput 
+import sys
+
 #Query user for beam radius
 beamRad=input('What is the beam radius?(mm) ')
 rtpath='\t\trtpath+='+beamRad+';\n'
 
 #Query used for filtered indices
-yes = set(['yes','y', 'ye', ''])
-skipBool=input('Do you need to remove extra indices (Y/n)? ').lower()
+yes = set(['yes','y', 'ye'])
+skipBool=input('Do you need to remove extra indices (y/N)? ').lower()
 if skipBool in yes:
     a=input('What indices should be filtered out? ').split(',')
     moda='\t\tif(i!=0'
@@ -16,19 +19,27 @@ if skipBool in yes:
 else:
     moda='\t\tif(i!=0){\n'
 
-#Open ggac analysis macro and create list by line number
-with open('angCorr.C','r') as file:
-    data=file.readlines()
+#Query for fitting
+yesFit = set(['yes','y', 'ye',''])
+fitBool=input('Do you want to fit the histogram (Y/n)? ').lower()
+if fitBool in yesFit:
+    fitMod='\t\tg->Fit("efit");\n'
+else:
+    fitMod='//\t\tg->Fit("efit");\n'
 
-#Change desired lines (linenumber-1)
-data[67]=data[134]=moda
-data[308]=rtpath
 
-#Write file
-with open('angCorr.C','w') as output_file:
-    output_file.writelines(data)
-   
-print()
-print()
-print("Done, \nMay you have long days and pleasant nights")
+#Reads input file line by line
+changeBool=False
+for line in fileinput.input(['angCorr.C'], inplace=True, backup='.bk'):
 
+#Changes beam radius for output .root file
+    if line.strip().startswith('rtpath+=') and changeBool==False:
+        line = rtpath
+        changeBool=True
+#Changes filtered indices
+    if line.strip().startswith('if(i!=0'):
+        line = moda
+#Toggles Fitting
+    if 'g->Fit(' in line:
+        line = fitMod
+    sys.stdout.write(line)
